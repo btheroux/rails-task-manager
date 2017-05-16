62ab4471d272f76b5b0425896e6fb9bf1206cbc4
(Brett Theroux) bretttheroux@gmail.com <bretttheroux@gmail.com>
2017-05-16 15:09:41 +0200

    dryer code for first exercise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, except: [:index, :new, :create]
+
   def index
     @tasks = Task.all
   end
@@ -33,6 +35,9 @@ class TasksController < ApplicationController
   end
 
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :details)
